35f31d05478982e9d689b48865bf1b37bee4a96a
Musa Zeshan <musazeshan.it@gmail.com>
2024-05-05 12:21:11 +0500

    I have understood the react flow and structure and know how to clean up unnecessary files and code from react projects whether it's been create with vite or without it.

---
+++ 01basicreact/src/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar() {
+  return (
+    <div className="w-100 h-12 bg-zinc-800 flex justify-between items-center text-white text-lg">
+      <div className="text-2xl font-semibold ms-2">React</div>
+      <div>
+        <ul className="flex gap-8">
+          <li>Home</li>
+          <li>Services</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div>
+        <button className="bg-black rounded-md px-4 py-[6px] me-4">Login</button>
+        <button className="bg-gray-600 rounded-md px-4 py-[6px]">Signup</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
