ce13378bc2c91044cad6b08f99294bb5cd76fd71
Musa Zeshan <musazeshan.it@gmail.com>
2024-05-08 17:17:16 +0500

    Made a background changer project and now working on the redux and redux toolkit.

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("bg-black");
+  const containerClasses = "h-screen w-screen flex items-center justify-center";
+  const containerWithBg = `${containerClasses} ${color}`;
+
+  function changeBgColor(colorName) {
+    setColor(colorName);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={containerWithBg}>
+        <div className="w-[80%] h-[80%] flex flex-col justify-end">
+          <h1 className="mx-auto text-4xl text-white mb-14 font-bold">
+            Background Changer
+          </h1>
+          <div className="bg-gray-100 px-5 py-2 rounded-lg flex justify-evenly mb-10">
+            <button
+              onClick={function () {
+                changeBgColor("bg-red-700");
+              }}
+              className="btn bg-red-600 text-white hover:bg-red-700 mx-2"
+            >
+              Red
+            </button>
+            <button
+              onClick={function () {
+                changeBgColor("bg-gray-700");
+              }}
+              className="btn bg-gray-600 text-white hover:bg-gray-700 mx-2"
+            >
+              Gray
+            </button>
+            <button
+              onClick={function () {
+                changeBgColor("bg-green-700");
+              }}
+              className="btn bg-green-600 text-white hover:bg-green-700 mx-2"
+            >
+              Green
+            </button>
+            <button
+              onClick={function () {
+                changeBgColor("bg-blue-700");
+              }}
+              className="btn bg-blue-600 text-white hover:bg-blue-700 mx-2"
+            >
+              Blue
+            </button>
+            <button
+              onClick={function () {
+                changeBgColor("bg-black");
+              }}
+              className="btn bg-zinc-900 text-white hover:bg-black mx-2"
+            >
+              Black
+            </button>
+            <button
+              onClick={function () {
+                changeBgColor("bg-pink-700");
+              }}
+              className="btn bg-pink-600 text-white hover:bg-pink-700 mx-2"
+            >
+              Pink
+            </button>
+            <button
+              onClick={function () {
+                changeBgColor("bg-yellow-500");
+              }}
+              className="btn bg-yellow-500 text-white hover:bg-yellow-600 mx-2"
+            >
+              Yellow
+            </button>
+            <button
+              onClick={function () {
+                changeBgColor("bg-purple-700");
+              }}
+              className="btn bg-purple-600 text-white hover:bg-purple-700 mx-2"
+            >
+              Purple
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
